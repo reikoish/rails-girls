d386ebf807880d90f08ca3f741bfb7b75824dd61
ReikoIshimaru <mathilda.v911@live.jp>
2020-03-29 14:15:08 +0900

    added comment function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_15_073149) do
+ActiveRecord::Schema.define(version: 2020_03_29_044959) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.integer "idea_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["idea_id"], name: "index_comments_on_idea_id"
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
@@ -32,4 +41,5 @@ ActiveRecord::Schema.define(version: 2020_02_15_073149) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "ideas"
 end
